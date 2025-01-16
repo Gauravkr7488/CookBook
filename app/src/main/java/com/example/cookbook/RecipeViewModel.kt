2431532d57b060be548997ace44288d7bb1d854e
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-16 11:22:07 +0530

    created viewmodel - RecipeViewModel.kt

---
+++ app/src/main/java/com/example/cookbook/RecipeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.cookbook
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class RecipeViewModel(private val dao: RecipeDao) : ViewModel() {
+    private val _recipes = MutableStateFlow<List<RecipeData>>(emptyList())
+    val recipes: StateFlow<List<RecipeData>> = _recipes
+
+    fun searchRecipe(query: String) {
+        viewModelScope.launch {
+            _recipes.value = dao.searchRecipeByName(query)
+        }
+    }
+}
