cdfda2912dab27871d8acad6ca1a0b87f2fac084
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-16 11:18:50 +0530

    Initialised the database using singleton - DatabaseProvider.kt

---
+++ app/src/main/java/com/example/cookbook/DatabaseProvider.kt
@@ -0,0 +1,19 @@
+package com.example.cookbook
+
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "recipes_database"
+            ).build()
+        }
+        return INSTANCE!!
+    }
+}
